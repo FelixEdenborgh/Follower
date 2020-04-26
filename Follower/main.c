705edb2cf49a45a65d6b64769c23bf9da03a4bcc
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 13:58:38 +0200

    update

---
+++ Follower/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+
+    printf("Welcome to the parrot!");
+    printf("Hows your day?: ");
+    scanf("%c", &human);
+
+    printf("Im an parrot and you said: ", human);
+
+
+    for(int i = 0; i < 10; i++){
+        printf("Im an parrot: ");
+        scanf("%c", &human1);
+        printf(human1);
+
+
+    }
+}
